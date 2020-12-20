301a1e3e55a8c3534d9c2bbd8e3a3b69b4868854
Connor Walden <connorwalden1403@gmail.com>
2020-12-20 22:38:09 +1030

    Added functionality for question two (third card)

--- js/script.js
+++ js/script.js
@@ -15,197 +15,263 @@ var isSwitching = false;
 
 // Trackers for how many the user got right
 var correctAnswers = 0;
+var numQuestions = 5;
 var log = [];
+
+// Current keeps track of what question we are up to
+var current = "intro";
 
 // Questions object to store all important data for the quiz.
 var questions = {
   "intro": {
     "title": "Code Quiz",
     "subtitle": "Are you ready?",
-    "question": "Are you ready to start the code quiz? For every answer just click one of the multiple choice answers to reply!",
+    "question":
+      "Are you ready to start the code quiz? For every answer just click one of the multiple choice answers to reply!",
     "answer1": "No, I'm all good :(",
     "answer2": "Yes, I'm going to destroy this!",
     "answer3": "disabled",
-    "answer4": "disabled"
+    "answer4": "disabled",
   },
   "one": {
     "title": "Code Quiz",
     "subtitle": "Question 1",
-    "question": "What is the HTML tag under which once can write the javascript code?",
+    "question":
+      "What is the HTML tag under which once can write the javascript code?",
     "answer1": "<javascript>",
     "answer2": "<scripted>",
     "answer3": "<script>",
-    "answer4": "<js>"
+    "answer4": "<js>",
+  },
+  "two": {
+    "title": "Code Quiz",
+    "subtitle": "Question 2",
+    "question": 
+      "Which of the following is the correct syntax to display \"GeeksForGeeks\" in an alert box using javascript?",
+    "answer1": "alertbox(\"GeeksForGeeks\");",
+    "answer2": "msg(\"GeeksForGeeks\");",
+    "answer3": "msgbox(\"GeeksForGeeks\");",
+    "answer4": "alert(\"GeeksForGeeks\");"
   }
-}
+};
 
 // Function that switches questions based on the state number provided, in almost all
 // cases, this number will go up by 1 when this function in invoked.
 function nextCard(num) {
-  if(num < 0 || num > 6)
-    return;
-  
+  if (num < 0 || num > 6) return;
+
   state = num;
 
-  switch(state) {
-    case 0: 
-      cardTitle.textContent = questions.intro.title;
-      cardSubtitle.textContent = questions.intro.subtitle;
-      cardQuestion.textContent = questions.intro.question;
-
-      // Check if any of the options for answers are diabled for this question, then disable them
-      if(questions.intro.answer1 !== "disabled") 
-        cardAns1.setAttribute("class", "btn btn-secondary button-fullwidth card-link");
-      else 
-        cardAns1.setAttribute("class", "btn btn-secondary button-fullwidth card-link disabled");
-
-      if(questions.intro.answer2 !== "disabled") 
-        cardAns1.setAttribute("class", "btn btn-secondary button-fullwidth card-link");
-      else 
-        cardAns1.setAttribute("class", "btn btn-secondary button-fullwidth card-link disabled");
-
-      if(questions.intro.answer3 !== "disabled") 
-        cardAns1.setAttribute("class", "btn btn-secondary button-fullwidth card-link");
-      else 
-        cardAns1.setAttribute("class", "btn btn-secondary button-fullwidth card-link disabled");
-
-      if(questions.intro.answer4 !== "disabled") 
-        cardAns1.setAttribute("class", "btn btn-secondary button-fullwidth card-link");
-      else 
-        cardAns1.setAttribute("class", "btn btn-secondary button-fullwidth card-link disabled");
-
-      // Set text content of each answer so the user know what to click 
-      cardAns1.textContent = questions.intro.answer1;
-      cardAns2.textContent = questions.intro.answer2;
-      cardAns3.textContent = questions.intro.answer3;
-      cardAns4.textContent = questions.intro.answer4;
-
-      // Reset any styling used for the previous question
-      cardAns1.removeAttribute("style");
-      cardAns2.removeAttribute("style");
-      cardAns3.removeAttribute("style");
-      cardAns4.removeAttribute("style");
-
-      isSwitching = false;
+  switch (state) {
+    case 0:
+      current = "intro";
       break;
-    case 1: 
-      cardTitle.textContent = questions.one.title;
-      cardSubtitle.textContent = questions.one.subtitle;
-      cardQuestion.textContent = questions.one.question;
-
-      // Check if any of the options for answers are diabled for this question, then disable them
-      if(questions.one.answer1 !== "disabled") 
-        cardAns1.setAttribute("class", "btn btn-secondary button-fullwidth card-link");
-      else 
-        cardAns1.setAttribute("class", "btn btn-secondary button-fullwidth card-link disabled");
-
-      if(questions.one.answer2 !== "disabled") 
-        cardAns2.setAttribute("class", "btn btn-secondary button-fullwidth card-link");
-      else 
-        cardAns2.setAttribute("class", "btn btn-secondary button-fullwidth card-link disabled");
-
-      if(questions.one.answer3 !== "disabled") 
-        cardAns3.setAttribute("class", "btn btn-secondary button-fullwidth card-link");
-      else 
-        cardAns3.setAttribute("class", "btn btn-secondary button-fullwidth card-link disabled");
-
-      if(questions.one.answer4 !== "disabled") 
-        cardAns4.setAttribute("class", "btn btn-secondary button-fullwidth card-link");
-      else 
-        cardAns4.setAttribute("class", "btn btn-secondary button-fullwidth card-link disabled");
-
-      cardAns1.textContent = questions.one.answer1;
-      cardAns2.textContent = questions.one.answer2;
-      cardAns3.textContent = questions.one.answer3;
-      cardAns4.textContent = questions.one.answer4;
-
-      // Reset any styling used for the previous question
-      cardAns1.removeAttribute("style");
-      cardAns2.removeAttribute("style");
-      cardAns3.removeAttribute("style");
-      cardAns4.removeAttribute("style");
-
-      isSwitching = false;
+    case 1:
+      current = "one";
+      break;
+    case 2:
+      current = "two";
+      break;
+    case 3:
+      current = "three";
+      break;
+    case 4:
+      current = "four";
+      break;
+    case 5:
+      current = "five";
+      break;
+    case 6:
+      current = "results";
       break;
   }
+
+  var curQuestion = questions[current];
+
+  cardTitle.textContent = curQuestion.title;
+  cardSubtitle.textContent = curQuestion.subtitle;
+  cardQuestion.textContent = curQuestion.question;
+
+  // Check if any of the options for answers are diabled for this question, then disable them
+  if (curQuestion.answer1 !== "disabled")
+    cardAns1.setAttribute(
+      "class",
+      "btn btn-secondary button-fullwidth card-link"
+    );
+  else
+    cardAns1.setAttribute(
+      "class",
+      "btn btn-secondary button-fullwidth card-link disabled"
+    );
+
+  if (curQuestion.answer2 !== "disabled")
+    cardAns2.setAttribute(
+      "class",
+      "btn btn-secondary button-fullwidth card-link"
+    );
+  else
+    cardAns2.setAttribute(
+      "class",
+      "btn btn-secondary button-fullwidth card-link disabled"
+    );
+
+  if (curQuestion.answer3 !== "disabled")
+    cardAns3.setAttribute(
+      "class",
+      "btn btn-secondary button-fullwidth card-link"
+    );
+  else
+    cardAns3.setAttribute(
+      "class",
+      "btn btn-secondary button-fullwidth card-link disabled"
+    );
+
+  if (curQuestion.answer4 !== "disabled")
+    cardAns4.setAttribute(
+      "class",
+      "btn btn-secondary button-fullwidth card-link"
+    );
+  else
+    cardAns4.setAttribute(
+      "class",
+      "btn btn-secondary button-fullwidth card-link disabled"
+    );
+
+  // Set text content of each answer so the user know what to click
+  cardAns1.textContent = curQuestion.answer1;
+  cardAns2.textContent = curQuestion.answer2;
+  cardAns3.textContent = curQuestion.answer3;
+  cardAns4.textContent = curQuestion.answer4;
+
+  // Reset any styling used for the previous question
+  cardAns1.removeAttribute("style");
+  cardAns2.removeAttribute("style");
+  cardAns3.removeAttribute("style");
+  cardAns4.removeAttribute("style");
+
+  isSwitching = false;
+  console.log("Correct Answers: " + correctAnswers);
 }
 
 // Functions that will determine whether user's response is correct using a js object for lookup
 function answer1() {
-  switch(state) {
+  isSwitching = true;
+
+  switch (state) {
     case 0:
-      if(!isSwitching) {
+      if (!isSwitching) {
         cardAns1.setAttribute("style", "background-color: red;");
-        isSwitching = true;
-        setTimeout(() => { nextCard(1) }, 2000);
+        setTimeout(() => {
+          nextCard(1);
+        }, 2000);
       }
       break;
     case 1:
-      if(!isSwitching) {
+      if (!isSwitching) {
         cardAns1.setAttribute("style", "background-color: red;");
-        isSwitching = true;
-        setTimeout(() => { nextCard(2) }, 2000);
+        setTimeout(() => {
+          nextCard(2);
+        }, 2000);
+      }
+      break;
+    case 2:
+      if (!isSwitching) {
+        cardAns1.setAttribute("style", "background-color: red;");
+        setTimeout(() => {
+          nextCard(3);
+        }, 2000);
       }
       break;
   }
 }
 
 function answer2() {
-  switch(state) {
-    case 0:
-      if(!isSwitching) {
-        cardAns2.setAttribute("style", "background-color: green;");
-        correctAnswers++;
-        isSwitching = true;
-        setTimeout(() => { nextCard(1); }, 2000);
-      }
-      break;
-    case 1:
-      if(!isSwitching) {
-        cardAns2.setAttribute("style", "background-color: red;");
-        isSwitching = true;
-        setTimeout(() => { nextCard(2); }, 2000);
-      }
-      break;
+  if (!isSwitching) {
+    switch (state) {
+      // Not increasing correct answers for the intro
+      case 0:
+          cardAns2.setAttribute("style", "background-color: green;");
+          setTimeout(() => {
+            nextCard(1);
+          }, 2000);
+        break;
+      case 1:
+          cardAns2.setAttribute("style", "background-color: red;");
+          setTimeout(() => {
+            nextCard(2);
+          }, 2000);
+        break;
+      case 2:
+          cardAns2.setAttribute("style", "background-color: red;");
+          setTimeout(() => {
+            nextCard(3);
+          }, 2000);
+        break;
+    }
+
+    isSwitching = true;
   }
 }
 
 function answer3() {
-  switch(state) {
-    case 0:
-      if(!isSwitching) {
-        cardAns3.setAttribute("style", "background-color: red;");
-        isSwitching = true;
-        setTimeout(() => { nextCard(1) }, 2000);
-      }
-      break;
-    case 1:
-      if(!isSwitching) {
-        cardAns3.setAttribute("style", "background-color: green;");
-        correctAnswers++;
-        isSwitching = true;
-        setTimeout(() => { nextCard(2) }, 2000);
-      }
-      break;
+  if(!isSwitching) {
+    switch (state) {
+      case 0:
+          cardAns3.setAttribute("style", "background-color: red;");
+          setTimeout(() => {
+            nextCard(1);
+          }, 2000);
+        break;
+      case 1:
+          cardAns3.setAttribute("style", "background-color: green;");
+          correctAnswers++;
+          setTimeout(() => {
+            nextCard(2);
+          }, 2000);
+        break;
+      case 2:
+          cardAns3.setAttribute("style", "background-color: red;");
+          setTimeout(() => {
+            nextCard(3);
+          }, 2000);
+        break;
+    }
+
+    isSwitching = true;
   }
 }
 
 function answer4() {
-  switch(state) {
-    case 0:
-      if(!isSwitching) {
-        cardAns4.setAttribute("style", "background-color: red;");
-        isSwitching = true;
-        setTimeout(() => { nextCard(1) }, 2000);
-      }
-      break;
-    case 1:
-      if(!isSwitching) {
-        cardAns4.setAttribute("style", "background-color: red;");
-        isSwitching = true;
-        setTimeout(() => { nextCard(2) }, 2000);
-      }
-      break;
+  if(!isSwitching) {
+    switch (state) {
+      case 0:
+        if (!isSwitching) {
+          cardAns4.setAttribute("style", "background-color: red;");
+          setTimeout(() => {
+            nextCard(1);
+          }, 2000);
+        }
+        break;
+      case 1:
+        if (!isSwitching) {
+          cardAns4.setAttribute("style", "background-color: red;");
+          setTimeout(() => {
+            nextCard(2);
+          }, 2000);
+        }
+        break;
+      case 2:
+        if (!isSwitching) {
+          cardAns4.setAttribute("style", "background-color: green;");
+          correctAnswers++;
+          setTimeout(() => {
+            nextCard(3);
+          }, 2000);
+        }
+        break;
+    }
+
+    isSwitching = true;
   }
 }
 
